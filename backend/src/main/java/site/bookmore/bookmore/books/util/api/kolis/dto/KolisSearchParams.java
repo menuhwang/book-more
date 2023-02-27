d6578079a68034fdab5b687ae7caec1052061e99
menuHwang <hmin0206@naver.com>
2023-02-27 17:19:25 +0900

    국립 중앙 도서관 도서 검색 api 수정

--- backend/src/main/java/site/bookmore/bookmore/books/util/api/kolis/dto/KolisSearchParams.java
+++ backend/src/main/java/site/bookmore/bookmore/books/util/api/kolis/dto/KolisSearchParams.java
@@ -1,12 +1,16 @@
 package site.bookmore.bookmore.books.util.api.kolis.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.bookmore.bookmore.books.util.api.BookSearchApiParams;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class KolisSearchParams {
+public class KolisSearchParams implements BookSearchApiParams {
     @Builder.Default
     private int page_no = 1;
     @Builder.Default
@@ -14,4 +18,10 @@ public class KolisSearchParams {
     private String isbn;
     private String title;
     private String author;
+
+    public static KolisSearchParams of(String isbn) {
+        return KolisSearchParams.builder()
+                .isbn(isbn)
+                .build();
+    }
 }
--- backend/src/main/java/site/bookmore/bookmore/books/util/api/kolis/dto/KolisSearchResponse.java
+++ backend/src/main/java/site/bookmore/bookmore/books/util/api/kolis/dto/KolisSearchResponse.java
@@ -2,18 +2,50 @@ package site.bookmore.bookmore.books.util.api.kolis.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import site.bookmore.bookmore.books.entity.Book;
+import site.bookmore.bookmore.books.util.api.BookSearchResponse;
+import site.bookmore.bookmore.books.util.mapper.BookMapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-public class KolisSearchResponse {
+public class KolisSearchResponse implements BookSearchResponse {
     @JsonProperty(value = "PAGE_NO")
     private String page;
     @JsonProperty(value = "TOTAL_COUNT")
     private String totalCount;
     private List<Doc> docs;
 
-    public long getTotalCount() {
+    private long getTotalCount() {
         return Long.parseLong(totalCount);
+    }
+
+    @Override
+    public Pageable getPageable() {
+        return PageRequest.of(Integer.parseInt(page), docs.size());
+    }
+
+    @Override
+    public Integer getSize() {
+        return docs.size();
+    }
+
+    @Override
+    public Long getTotal() {
+        return getTotalCount();
+    }
+
+    @Override
+    public Book getFirst() {
+        if (docs.size() == 0) return null;
+        return BookMapper.of(docs.get(0));
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return docs.stream().map(BookMapper::of).collect(Collectors.toList());
     }
 }
