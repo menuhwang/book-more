c9a49ea62f2b5b8f90d74547ea580daec7ac8a28
menuHwang <hmin0206@naver.com>
2023-02-27 17:20:44 +0900

    파일 위치 이동

--- backend/src/main/java/site/bookmore/bookmore/books/config/BookCrawlerConfig.java
+++ backend/src/main/java/site/bookmore/bookmore/books/config/BookCrawlerConfig.java
@@ -1,4 +1,4 @@
-package site.bookmore.bookmore.config;
+package site.bookmore.bookmore.books.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
